b4583abeb312d81b529b20c6cf5728016a7980d1
Alexandru Burghelea <burghelea.alex@gmail.com>
2017-01-19 00:21:58 +0200

    Update 10 Mail funtion

--- app/src/main/java/com/example/alexandru/justjava/MainActivity.java
+++ app/src/main/java/com/example/alexandru/justjava/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.alexandru.justjava;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -41,7 +42,40 @@ public class MainActivity extends AppCompatActivity {
         String message = createOrderSummary();
         displayMessage(message);
 
+        composeEmail(new String[1], "", "");
+
+//        Intent intent = new Intent(Intent.ACTION_VIEW);
+//        intent.setData(Uri.parse("geo:47.6, -122.3"));
+//        if (intent.resolveActivity(getPackageManager()) != null) {
+//            Log.i("msg","here in if");
+//            startActivity(intent);
+//        }
+
+
     }
+
+    /**
+     * @param addresses the email addresses to send
+     * @param subject   the subject of the email
+     * @param text      the text of the mail
+     */
+
+    public void composeEmail(String[] addresses, String subject, String text) {
+
+        addresses[0] = "burghelea.alex@gmail.com";
+        subject = "Coffee order ";
+        text = createOrderSummary();
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
 
     /**
      * Calculates the price of the order based on the current quantity.
@@ -84,11 +118,15 @@ public class MainActivity extends AppCompatActivity {
         if (quantity < 100) {
             quantity++;
         } else {
+            //get the context
             Context context = getApplicationContext();
+            // set the text to display
             CharSequence text = "You cannot have more than 100 coffees";
+            // the length of time will stay on screen
             int duration = Toast.LENGTH_SHORT;
-
+            // creating a toast
             Toast toast = Toast.makeText(context, text, duration);
+            // making the toast display
             toast.show();
             return;
         }
@@ -103,11 +141,15 @@ public class MainActivity extends AppCompatActivity {
         if (quantity > 1) {
             quantity--;
         } else {
+            //get the context
             Context context = getApplicationContext();
+            // set the text to display
             CharSequence text = "You cannot have less than 1 coffee";
+            // the length of time will stay on screen
             int duration = Toast.LENGTH_SHORT;
-
+            // creating a toast
             Toast toast = Toast.makeText(context, text, duration);
+            // making the toast display
             toast.show();
             return;
         }
